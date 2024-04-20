9662838fac649131b7fd3af4bf2fbf284d3086c8
aymo70 <aymanramzi70@gmail.com>
2024-04-20 09:16:17 -0700

    start app

---
+++ android/app/src/main/kotlin/com/example/aymostoreapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aymostoreapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:aymostoreapp/aymo_store_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AymoStoreApp());
+}
