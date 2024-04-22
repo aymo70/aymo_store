5cea89cd20ad3810b43e764ca3e9473ea7c39e02
aymo70 <aymanramzi70@gmail.com>
2024-04-22 12:41:54 -0700

    env created done

--- lib/aymo_store_app.dart
+++ lib/aymo_store_app.dart
@@ -1,3 +1,4 @@
+import 'package:aymostoreapp/core/app/env.variable.dart';
 import 'package:flutter/material.dart';
 
 class AymoStoreApp extends StatelessWidget {
@@ -8,6 +9,7 @@ class AymoStoreApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aymo Store',
+      debugShowCheckedModeBanner: EnvVariable.instance.debug,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
